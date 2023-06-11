62c05dd7af0f79a942e38acd1d9ee283307b2595
Nishy baby <nischal.bhatt.87@gmail.com>
2023-06-11 10:39:46 +0800

    test

--- helloworld/src/main/java/com/lab/config/BatchConfiguration.java
+++ helloworld/src/main/java/com/lab/config/BatchConfiguration.java
@@ -2,6 +2,7 @@ package com.lab.config;
 
 import com.lab.listener.HelloWorldJobExecutionListener;
 import com.lab.listener.HelloWorldStepExecutionListener;
+import com.lab.model.Product;
 import com.lab.processor.InMemeItemProcessor;
 import com.lab.reader.InMemeReader;
 import com.lab.writer.ConsoleItemWriter;
@@ -11,15 +12,24 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.nio.file.FileSystem;
 
 @EnableBatchProcessing
 @Configuration
@@ -74,8 +84,9 @@ public class BatchConfiguration {
     public Step step3(){
         return stepBuilderFactory.get("step2")
                 .<Integer,Integer>chunk(3)
-                .reader(reader())
-                .processor(inMemeItemProcessor)
+                //.reader(reader())
+                .reader(flatFileItemReader(null))
+                //.processor(inMemeItemProcessor)
                 .writer(new ConsoleItemWriter())
                 .build();
     }
@@ -87,6 +98,37 @@ public class BatchConfiguration {
         return new InMemeReader();
     }
 
+    @StepScope
+    @Bean
+    public FlatFileItemReader flatFileItemReader(
+            @Value("#{jobParameters['fileInput']}")
+            FileSystemResource inputFile){
+           FlatFileItemReader reader = new FlatFileItemReader();
+           reader.setResource(inputFile);
+           reader.setLineMapper(
+                   new DefaultLineMapper<Product>(){
+                       {
+                           setLineTokenizer(new DelimitedLineTokenizer(){
+                               {
+                                   setNames(new String[]{"productID","productName","productDesc","price","unit"});
+                                   setDelimiter("|");
+
+
+                               }
+
+                           });
+
+                           setFieldSetMapper(new BeanWrapperFieldSetMapper<Product>(){
+                               {
+                                   setTargetType(Product.class);
+                               }
+                           });
+                       }
+                   });
+           reader.setLinesToSkip(1);
+           return reader;
+    }
+
 
 
     @Bean
@@ -94,6 +136,7 @@ public class BatchConfiguration {
         return stepBuilderFactory.get("step2")
                 .listener(helloWorldStepExecutionListener)
                 .tasklet(helloWorldTasklet2()).build();
+
     }
 
 
---
+++ helloworld/src/main/java/com/lab/model/Product.java
@@ -0,0 +1,63 @@
+package com.lab.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Integer productId;
+    private String productName;
+    private String productDesc;
+    private BigDecimal price;
+    private Integer unit;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Integer unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDesc='" + productDesc + '\'' +
+                ", price=" + price +
+                ", unit=" + unit +
+                '}';
+    }
+}
